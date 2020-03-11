d86290d2b8ef4eb7b27478863984f4fcd880a50f
apostoloskotsiras <46163250+apostoloskotsiras@users.noreply.github.com>
2020-03-11 08:54:28 +0200

    Update en.lua
    word corection

--- locales/en.lua
+++ locales/en.lua
@@ -4,6 +4,6 @@ Locales['en'] = {
     ['alreadyHair'] = 'Someone hairing please try later!',
     ['angle'] = '~INPUT_VEH_FLY_YAW_LEFT~ Default View ~INPUT_VEH_FLY_YAW_RIGHT~ Side View',
     ['menu_title'] = 'Change Style',
-    ['buttom_Help'] = '~INPUT_FRONTEND_ENDSCREEN_ACCEPT~ ~b~Confirm and leave\n~INPUT_SELECT_CHARACTER_MICHAEL~ Change hairstyle - ~g~ %s$\n~INPUT_SELECT_CHARACTER_FRANKLIN~ ~b~Change beardstyle - ~g~ %s$\n~INPUT_SELECT_CHARACTER_TREVOR~ ~b~Trim your eyebrows - ~g~ %s$\n~b~Total free: ~g~%s$',
+    ['buttom_Help'] = '~INPUT_FRONTEND_ENDSCREEN_ACCEPT~ ~b~Confirm and leave\n~INPUT_SELECT_CHARACTER_MICHAEL~ Change hairstyle - ~g~ %s$\n~INPUT_SELECT_CHARACTER_FRANKLIN~ ~b~Change beardstyle - ~g~ %s$\n~INPUT_SELECT_CHARACTER_TREVOR~ ~b~Trim your eyebrows - ~g~ %s$\n~b~Total fee: ~g~%s$',
     ['paid'] = 'You paid %s$',
 }
